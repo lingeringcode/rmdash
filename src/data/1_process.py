e257b5c61bf04dcfa4213595d4317a1d1c18af98
Chris Lindgren <chris.a.lindgren@gmail.com>
2025-09-21 13:52:06 -0400

    update with latest 2025-26 jobs; cleaned position and URL strings

--- src/data/1_process.py
+++ src/data/1_process.py
@@ -7,7 +7,6 @@ df_latlon = pd.read_csv("lat-lon-references.csv")
 
 '''
 @process_data(jdata, geodata)
-
 '''
 def process_data(geodata, jdata):
   '''
@@ -36,9 +35,9 @@ def process_data(geodata, jdata):
         new_row = {
           'AY':job['AY'],
           'Institution':job['Institution'],
-          'Position':job['Position'],
+          'Position': job['Position'].replace("\n", ""),
           'Address':job['Address'],
-          'Link':job['Link'],
+          'Link':job['Link'].replace("\n", ""),
           'TrackType':job['TrackType'],
           'DateAdded':job['DateAdded'],
           'DateTimeObj':datetime_object,
@@ -80,10 +79,69 @@ def process_data(geodata, jdata):
   return df_new_sorted
 
 
-
 df_combine_data = process_data(df_latlon, df_og)
-print(df_combine_data.tail().values)
-print(
-  df_combine_data.info()
-)
+print( df_combine_data.tail().values )
+print( df_combine_data.info() )
+
 df_combine_data.to_csv('combined-jobs.csv', index=False)
+
+def create_subset(df, subset_col_1, subset_col_dates):
+  '''
+  # @create_subset: Create subsets of TrackType data
+
+  # Params
+  - df: pandas DataFrame. Data set with all parameters of interest
+  - subset_col: String. Column of interest.
+
+  ## Return
+  - new_df: pandas DataFrame. New dataframe with following structure per row:
+    - date: DateTime Obj of posting date.
+    - type: String. Type of track: TT, NTT, Unavailable.
+    - posts: Integer. Total sum of posts of that type of job on that specific date.
+  '''
+  uniq_subset_col_values = df[subset_col_1].unique()
+  uniq_subset_col_dates = sorted(df[subset_col_dates].apply(lambda d: datetime.strptime(d,'%m/%d/%Y')).unique())
+  list_unique_dates_sorted = []
+  for date in uniq_subset_col_dates:
+    list_unique_dates_sorted.append(date.strftime("%m/%d/%Y"))
+
+  list_record_rows_per_date = []
+  list_record_rows_flat = []
+  for date_str in list_unique_dates_sorted:
+    if date_str != '01/01/1900':
+      date_slice = df.loc[df.DateAdded == date_str]
+      list_record_rows_per_date.append({
+        'date': date_str,
+        'datetimeObj': datetime.strptime(date_str, '%m/%d/%Y'),
+        'TT': len(date_slice.loc[date_slice.TrackType == 'TT']),
+        'NTT': len(date_slice.loc[date_slice.TrackType == 'NTT']),
+        'Unavailable': len(date_slice.loc[date_slice.TrackType == 'Unavailable']),
+        'Total': len(date_slice.loc[date_slice.TrackType == 'TT'])+len(date_slice.loc[date_slice.TrackType == 'NTT'])+len(date_slice.loc[date_slice.TrackType == 'Unavailable'])
+      })
+
+      for tt in uniq_subset_col_values:
+        tt_date_slice = date_slice.loc[date_slice.TrackType == tt]
+        list_record_rows_flat.append({
+          'date': date_str,
+          'datetimeObj': datetime.strptime(date_str, '%m/%d/%Y'),
+          'type': tt,
+          'posts': len(tt_date_slice)
+        })
+  df_new_flat = pd.DataFrame(list_record_rows_flat)
+  df_new_per_date = pd.DataFrame(list_record_rows_per_date)
+  # Add moving averages
+  df_new_flat['Overall_EMA'] = df_new_flat['posts'].ewm(span=7).mean()        # Exponential Moving Average
+  df_new_per_date['Overall_EMA'] = df_new_per_date['Total'].ewm(span=7).mean()        # Exponential Moving Average
+  return {
+    'df_new_flat': df_new_flat,
+    'df_new_per_date': df_new_per_date,
+  }
+
+df__tt_per_date_flat = create_subset(
+  df_combine_data,
+  subset_col_1='TrackType',
+  subset_col_dates='DateAdded'
+)
+
+df__tt_per_date_flat['df_new_flat'].to_csv('tt-per-date-flat.csv', index=False)
+df__tt_per_date_flat['df_new_per_date'].to_csv('tt-per-date.csv', index=False)
